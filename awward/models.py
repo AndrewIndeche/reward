ac80d6c0df13661bc26f9be3116fb730de31e52d
AndrewIndeche <indecheandrew@gmail.com>
2021-10-26 15:43:27 +0300

    add profile class to model

--- awward/models.py
+++ awward/models.py
@@ -3,3 +3,11 @@ import datetime as dt
 from django.contrib.auth.models import User
 
 # Create your models here.
+Class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    profile_picture = models.ImageField(upload_to = 'images/',default='alaska.jpg')
+    name = models.CharField(blank=True,max_length=60)
+    bio = models.TextField(blank=True,max_length=500)
+
+Class Post (models.Model):
+Class Rate (models.Model):
